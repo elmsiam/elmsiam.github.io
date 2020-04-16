a09febc4f64108213d1d276797cf67668522eed6
elmsiam <63725944+elmsiam@users.noreply.github.com>
2020-04-16 12:18:18 -0400

    1.2

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const images = [
       src:
         './images/p1.png',
       desc: '15.5x12 inch painting on a wooden canvas',
-      height: '600',
       price: '$40.00 CAD'
     },
     {
@@ -18,7 +17,6 @@ const images = [
       src:
         './images/p2.png',
       desc: '9x12 inch painting on a cotton canvas board',
-      height: '800',
       price: '$20.00 CAD'
     },
     {
@@ -28,7 +26,6 @@ const images = [
       src:
         './images/p3.png',
       desc: '9x12 inch painting on a cotton canvas board',
-      height: '800',
       price: '$20.00 CAD'
     },
     {
@@ -38,7 +35,6 @@ const images = [
       src:
         './images/p4.png',
       desc: '15.5x12 inch painting on a wooden canvas',
-      height: '600',
       price: '$25.00 CAD'
     },
     {
@@ -48,7 +44,6 @@ const images = [
       src:
         './images/p5.png',
       desc: '5x7 inch painting on a cotton canvas board',
-      height: '800',
       price: '$10.00 CAD'
     },
     {
@@ -58,7 +53,6 @@ const images = [
       src:
         './images/p6.png',
       desc: '8x10 inch painting on a wooden canvas',
-      height: '600',
       price:'$15.00 CAD'
     },
     {
@@ -68,7 +62,6 @@ const images = [
       src:
         './images/p7.png',
       desc: '9x12 inch painting on a cotton canvas board',
-      height: '800',
       price:'$20.00 CAD'
     },
     {
@@ -78,7 +71,6 @@ const images = [
       src:
         './images/p8.png',
       desc: '9x12 inch painting on a cotton canvas board',
-      height: '600',
       price: '$20.00 CAD'
     },
     {
@@ -88,7 +80,6 @@ const images = [
       src:
         './images/p9.png',
       desc: '8x10 inch painting on a wooden canvas',
-      height: '800',
       price: '$15.00 CAD'
     },
     {
@@ -98,7 +89,6 @@ const images = [
       src:
         './images/p10.png',
       desc: '5x7 inch painting on a cotton canvas board',
-      height: '800',
       price: '$10.00 CAD'
     }
 ];
@@ -122,6 +112,7 @@ function chooseItems (itemType){
         var titleH = document.createElement("h3");
         titleH.innerText = "Abstract Paintings";
         document.getElementById("itemscontainer").appendChild(titleH);
+        document.getElementById("itemscontainer").innerHTML += "<br><br><br>";
         //show only abstract paintings
         for (var i in images) {
             if(images[i].type=='Abstract Painting'){
@@ -191,7 +182,7 @@ function chooseItems (itemType){
 function showWebDesc(){
   var bio = document.getElementById("bio");
 
-  bio.innerHTML += "<p id='bbio'>" + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non orci sed arcu porttitor tempus vel in dui. Praesent euismod, enim eget maximus lobortis, tellus nunc iaculis lorem, egestas congue diam felis ac orci. Nulla nec justo non sem pretium accumsan. Nullam feugiat eleifend nunc rutrum imperdiet. Maecenas a elementum magna. In id mi eros. Etiam interdum rhoncus felis, sed vehicula ante dignissim ac. Proin et pulvinar mi, ut efficitur tortor. Nullam suscipit rhoncus ultricies. Proin posuere, urna id rutrum rhoncus, lacus massa sollicitudin nisi, a luctus mauris diam commodo lorem.<br>"+"</p>";
+  bio.innerHTML += "<p id='bbio'>" + "Liam Morris is a painter who has been painting since the young age of two months. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod, enim eget maximus lobortis, tellus nunc iaculis lorem, egestas congue diam felis ac orci. Nulla nec justo non sem pretium accumsan. Nullam feugiat eleifend nunc rutrum imperdiet. Maecenas a elementum magna. In id mi eros. Etiam interdum rhoncus felis, sed vehicula ante dignissim ac. Proin et pulvinar mi, ut efficitur tortor. Nullam suscipit rhoncus ultricies. Proin posuere, urna id rutrum rhoncus, lacus massa sollicitudin nisi, a luctus mauris diam commodo lorem.<br>"+"</p>";
   bio.innerHTML += "<p id='bbbio'>" + "Cras velit ante, viverra placerat ipsum eget, rutrum ornare mauris. Cras at orci a orci maximus tincidunt. Suspendisse accumsan vestibulum dui. Suspendisse vitae pellentesque nisl. Nam leo arcu, varius ut sodales nec, sollicitudin a ipsum. Vivamus luctus, dui nec efficitur semper, sapien tortor faucibus turpis, eget lacinia urna sem eget augue. Ut finibus ultricies nibh id semper." + "</p>";
 };
 
